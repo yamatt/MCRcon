91d7a34ee1cafe4e53acd8c6998599587c527a9e
Matthew Copperwaite <mattcopp@gmail.com>
2012-08-11 16:35:47 +0100

    Refactored and added extra functions for common commands

--- mcrcon.py
+++ mcrcon.py
@@ -4,32 +4,81 @@ import struct
 import re
 
 class MCRcon:
+	GAMEMODE_SURVIVAL = 0
+	GAMEMODE_CREATIVE = 1
+	GAMEMODE_ADVENTURE = 2
+	
     def __init__(self, host, port, password):
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.s.connect((host, port))
-        self.send_real(3, password)
+        self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__socket.connect((host, port))
+        self._send(3, password)
+        
+    def save_all(self):
+		return self.send("save-all")
+		
+	def save_off(self):
+		return self.send("save-off")
+		
+	def save_on(self):
+		return self.send("save-on")
+		
+	def say(self, message):
+		return self.send("say %s" % message)
+		
+	def stop(self):
+		return self.send("stop")
+		
+	def give(self, player, item, amount=1, damage=0):
+		return self.send("give %s" % " ".join([player, item, amount, damage]))
+		
+	def whitelist_on(self):
+		return self.send("whitelist on")
+		
+	def whitelist_off(self):
+		return self.send("whitelist off")
+		
+	def whitelist_add(self, name, reload_list=True):
+		message = self.send("whitelist add %s" % name)
+		if reload_list:
+			message += self.whitelist_reload()
+		return message
+		
+	def whitelist_remove(self, name, reload_list=True):
+		message = self.send("whitelist remove %s" % name)
+		if reload_list:
+			message += self.whitelist_reload()
+		return message
+		
+	def whitelist_reload(self):
+		return self.send("whitelist reload")
+		
+	def gamemode(self, mode, player=None):
+		if not player:
+			return self.send("gamemode %d" % mode)
+		else:
+			return self.send("gamemode %d %s" % (mode, player))
     
     def close(self):
-        self.s.close()
+        self.__socket.close()
     
     def send(self, command):
-        return self.send_real(2, command)
+        return self._send(2, command)
     
-    def send_real(self, out_type, out_data):
+    def _send(self, out_type, out_data):
         #Send the data
         buff = struct.pack('<iii', 
             10+len(out_data),
             0,
             out_type) + out_data + "\x00\x00"
-        self.s.send(buff)
+        self.__socket.send(buff)
         
         #Receive a response
         in_data = ''
         ready = True
         while ready:
             #Receive an item
-            tmp_len, tmp_req_id, tmp_type = struct.unpack('<iii', self.s.recv(12))
-            tmp_data = self.s.recv(tmp_len-8) #-8 because we've already read the 2nd and 3rd integer fields
+            tmp_len, tmp_req_id, tmp_type = struct.unpack('<iii', self.__socket.recv(12))
+            tmp_data = self.__socket.recv(tmp_len-8) #-8 because we've already read the 2nd and 3rd integer fields
 
             #Error checking
             if tmp_data[-2:] != '\x00\x00':
@@ -50,6 +99,6 @@ class MCRcon:
             in_data += tmp_data
 
             #Check if more data ready...
-            ready = select.select([self.s], [], [], 0)[0]
+            ready = select.select([self.__socket], [], [], 0)[0]
         
         return in_data
